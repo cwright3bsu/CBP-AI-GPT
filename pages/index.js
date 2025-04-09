799350ae27beb72b2b4ef33b780abba49f304f0e
cwright3bsu <cwright3@bsu.edu>
2025-04-09 10:11:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import InterviewComponent from '../components/InterviewComponent';
+import Interview from '../components/Interview';
 
 export default function Home() {
   return (
-    <div>
-      <h1>CBP GPT Interview Simulator</h1>
-      <InterviewComponent />
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}>
+        CBP GPT Interview Simulator
+      </h1>
+      <Interview />
     </div>
   );
 }
